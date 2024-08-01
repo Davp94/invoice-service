7dfc33559b1199767e8517a8ea894b996a19a9e1
David <davp123.dh@gmail.com>
2024-08-01 00:07:57 +0000

    add swagger and instal netjs jwt

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder().setTitle('API INVOICE').setDescription('API INVOICE TO BLUMBIT REACT NEXTJS COURSE').setVersion('1.0').addTag('invoice').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
