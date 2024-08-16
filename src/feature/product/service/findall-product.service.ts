ae8c0ff4a098c7999f69dd869d4df303d81f74f3
David <davp123.dh@gmail.com>
2024-08-16 00:40:56 +0000

    improvements pagination

--- src/feature/product/service/findall-product.service.ts
+++ src/feature/product/service/findall-product.service.ts
@@ -31,15 +31,25 @@ export class FindallProductService {
   }
 
 
-  async getAllProductsPagination(manager: EntityManager, paginationSortingDto: PaginationSortingDto): Promise<ProductDto[]> {
+  async getAllProductsPagination(manager: EntityManager, paginationSortingDto: PaginationSortingDto): Promise<any> {
     const products: Product[] = await manager.find(Product, {
       relations: { category: true },
       order: { [paginationSortingDto.sortParam]: paginationSortingDto.sortDirection },
       take: Number(paginationSortingDto.take),
       skip: paginationSortingDto.take * paginationSortingDto.page,
+      where: {},
     });
+
+    const allProducts: Product[] = await manager.find(Product);
+    let totalRecords = 0;
+    allProducts.map(product => totalRecords++);
+    let totalPages = totalRecords / paginationSortingDto.take;
+    const result = totalPages - Math.floor(totalPages) !== 0;
+    if (result && Number(totalPages.toString().split('.')[1]) < 5) {
+      totalPages++;
+    }
     // take: 10 -- page: 0      0-10
     // take: 10 -- page: 1      11-20
-    return products;
+    return { content: products, metadata: { total: totalRecords, pages: Math.round(totalPages) } };
   }
 }
